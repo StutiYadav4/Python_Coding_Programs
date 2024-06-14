5c80a50be172d7e7979f47b93cb01c569658944b
Stuti Yadav <yadavstuti286@gmail.com>
2024-06-14 11:15:11 +0530

    Add files via upload

---
+++ SortingStringAsPerFrequency.py
@@ -0,0 +1,37 @@
+'''Given a string s, sort it in decreasing order based on the frequency of the characters. The frequency of a character is the number of times it appears in the string.
+
+Return the sorted string. If there are multiple answers, return any of them.
+
+ Example 1:
+
+Input: s = "tree"
+Output: "eert"
+Explanation: 'e' appears twice while 'r' and 't' both appear once.
+So 'e' must appear before both 'r' and 't'. Therefore "eetr" is also a valid answer.
+Example 2:
+
+Input: s = "cccaaa"
+Output: "aaaccc"
+Explanation: Both 'c' and 'a' appear three times, so both "cccaaa" and "aaaccc" are valid answers.
+Note that "cacaca" is incorrect, as the same characters must be together.
+Example 3:
+
+Input: s = "Aabb"
+Output: "bbAa"
+Explanation: "bbaA" is also a valid answer, but "Aabb" is incorrect.
+Note that 'A' and 'a' are treated as two different characters.'''
+
+
+inputStr = input("Enter the string :")
+opStr = ""
+opDict = {}
+for i in inputStr:
+    opDict[i] = 0
+for i in inputStr:
+    opDict[i] += 1
+opDict = sorted(opDict.items(), key=lambda x: x[1], reverse=True)
+for ind in opDict:
+    for j in range(ind[1]):
+        opStr += ind[0]
+print(opStr)
+
